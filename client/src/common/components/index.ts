b8d1d9a9d16bf2dec26151167adc886bc326f804
bonatolasa <tolboni614@gmail.com>
2026-04-30 14:42:21 +0300

    index commit

---
+++ client/src/common/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Sidebar } from './Sidebar';
+export { default as Navbar } from './Navbar';
+export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as Loader } from './Loader';
+export { Modal } from './Modal';
+export { Can } from './Can';
